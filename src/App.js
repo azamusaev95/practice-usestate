6cd8830bd3ea77a15b3f9ae65c71e9abde7fd5ea
azamusaev95 <azamatamerika2@gmail.com>
2022-07-19 10:32:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const data = [
+    {
+      id: "el1",
+      title: "NextJS",
+      checked: false,
+    },
+    {
+      id: "el2",
+      title: "ReactJs",
+      checked: false,
+    },
+    {
+      id: "el3",
+      title: "JS",
+      checked: false,
+    },
+  ];
+
+  return <div className="App">Code</div>;
 }
 
 export default App;
